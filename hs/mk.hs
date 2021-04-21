1353f70827523715d4a82c553aef4519480ead7f
rd <rd@slavepianos.org>
2021-04-21 03:54:26 +0000

    +help/graph/f0-tw-

--- hs/mk.hs
+++ hs/mk.hs
@@ -4,7 +4,7 @@ uop :: [(String, Int)]
 uop =
   [("negated",0),("abs",5)
   ,("frac",10),("cubed",13),("reciprocal",16),("midicps",17)
-  ,("log",25),("sin",28)
+  ,("log",25),("sin",28),("tanh",36)
   ,("distort",42)]
 
 binop :: [(String, Int)]
@@ -17,15 +17,17 @@ binop =
 -- > Data.List.sort ugen == ugen
 ugen :: [String]
 ugen =
-  ["AllpassN","AmpComp","Amplitude","BPF","BPZ2","BrownNoise","CombL","CombN","Crackle"
+  ["AllpassN","AmpComp","Amplitude","BPF","BPZ2","BrownNoise","CombC","CombL","CombN","Crackle"
   ,"Decay","Decay2","DegreeToKey","DelayN","Demand","Drand","Dseq","Dshuf","Dust","Duty"
-  ,"EnvGen","ExpRand","FSinOsc","FreeVerb","GVerb","HPZ1","IRand","Impulse","In","Klank"
+  ,"EnvGen","ExpRand","Formant","FSinOsc","FreeVerb","GVerb","HPF","HPZ1"
+  ,"IRand","Impulse","In","InFeedback","Klank"
   ,"LFNoise1","LFNoise2","LFPulse","LFSaw","LFTri","LPF"
-  ,"Lag","Latch","LeakDC","Line","LocalBuf","LocalIn","LocalOut"
+  ,"Lag","LagUD","Lag2","Lag3","Latch","LeakDC","Line","LinExp","LocalBuf","LocalIn","LocalOut"
   ,"ModDif","MouseX","MouseY","MulAdd","OnePole","Out"
-  ,"Pan2","Phasor","PinkNoise","Pulse","PulseDivider"
-  ,"RHPF","RLPF","Rand","Resonz","SampleDur","Saw","SetBuf","SetResetFF","SinOsc","SinOscFB"
-  ,"Trig","WhiteNoise","XFade2","XLine"]
+  ,"Pan2","Phasor","PinkNoise","Pluck","Pulse","PulseDivider"
+  ,"RHPF","RLPF","Rand","Resonz","Ringz","RunningMax"
+  ,"SampleDur","Saw","Select","SetBuf","SetResetFF","SinOsc","SinOscFB","Sweep"
+  ,"Trig","VarSaw","WhiteNoise","XFade2","XLine"]
 
 main :: IO ()
 main = st_sc3_gen_bindings_wr "/home/rohan/sw/stsc3/st/SC3-UGen.st" uop binop ugen
