893f134209a598722b6afa40d6914b36b457bb81
rohan <rd>
2021-08-14 14:58:51 +1000

    +Language/Smalltalk/Ansi/Rewrite

--- Language/Smalltalk/Ansi/Annotate.hs
+++ Language/Smalltalk/Ansi/Annotate.hs
@@ -1,80 +1,14 @@
 -- | Ast annotation functions.
 module Language.Smalltalk.Ansi.Annotate where
 
-import Data.Bifunctor {- base -}
-
-import Language.Smalltalk.Ansi
-
--- * Method Blocks
+import Language.Smalltalk.Ansi {- stsc3 -}
+import Language.Smalltalk.Ansi.Rewrite {- stsc3 -}
 
 -- | Assign blockMethodName to all blocks defined within MethodDefinition.
 methodDefinitionAnnotateBlocks :: MethodDefinition -> MethodDefinition
 methodDefinitionAnnotateBlocks m =
-  let nm = methodName m
-  in m {methodStatements = fmap (statementsAnnotateBlocks nm) (methodStatements m)}
-
-statementsAnnotateBlocks :: MethodName -> Statements -> Statements
-statementsAnnotateBlocks nm st =
-  case st of
-    StatementsReturn ret ->
-      StatementsReturn (returnStatementAnnotateBlocks nm ret)
-    StatementsExpression expr mst ->
-      StatementsExpression (expressionAnnotateBlocks nm expr) (fmap (statementsAnnotateBlocks nm) mst)
-
-returnStatementAnnotateBlocks :: MethodName -> ReturnStatement -> ReturnStatement
-returnStatementAnnotateBlocks nm (ReturnStatement expr) =
-  ReturnStatement (expressionAnnotateBlocks nm expr)
-
-expressionAnnotateBlocks :: MethodName -> Expression -> Expression
-expressionAnnotateBlocks nm expr =
-  case expr of
-    ExprAssignment as -> ExprAssignment (assignmentAnnotateBlocks nm as)
-    ExprBasic be -> ExprBasic (basicExpressionAnnotateBlocks nm be)
-
-assignmentAnnotateBlocks :: MethodName -> Assignment -> Assignment
-assignmentAnnotateBlocks nm (Assignment k expr) =
-  Assignment k (expressionAnnotateBlocks nm expr)
-
-basicExpressionAnnotateBlocks :: MethodName -> BasicExpression -> BasicExpression
-basicExpressionAnnotateBlocks nm (BasicExpression p m cm) =
-  BasicExpression
-  (primaryAnnotateBlocks nm p)
-  (fmap (messagesAnnotateBlocks nm) m)
-  (fmap (cascadedMessagesAnnotateBlocks nm) cm)
-
-primaryAnnotateBlocks :: MethodName -> Primary -> Primary
-primaryAnnotateBlocks nm p =
-  case p of
-    PrimaryBlock b -> PrimaryBlock (b {blockMethodName = Just nm})
-    PrimaryExpression e -> PrimaryExpression (expressionAnnotateBlocks nm e)
-    PrimaryArrayExpression a -> PrimaryArrayExpression (map (basicExpressionAnnotateBlocks nm) a)
-    _ -> p
-
-messagesAnnotateBlocks :: MethodName -> Messages -> Messages
-messagesAnnotateBlocks nm m =
-  case m of
-    MessagesUnary u b k ->
-      MessagesUnary u (fmap (map (binaryMessageAnnotateBlocks nm)) b) (fmap (keywordMessageAnnotateBlocks nm) k)
-    MessagesBinary b k ->
-      MessagesBinary (map (binaryMessageAnnotateBlocks nm) b) (fmap (keywordMessageAnnotateBlocks nm) k)
-    MessagesKeyword k ->
-      MessagesKeyword (keywordMessageAnnotateBlocks nm k)
-
-binaryMessageAnnotateBlocks :: MethodName -> BinaryMessage -> BinaryMessage
-binaryMessageAnnotateBlocks nm (BinaryMessage k a) =
-  BinaryMessage k (binaryArgumentAnnotateBlocks nm a)
-
-binaryArgumentAnnotateBlocks :: MethodName -> BinaryArgument -> BinaryArgument
-binaryArgumentAnnotateBlocks nm (BinaryArgument p u) =
-  BinaryArgument (primaryAnnotateBlocks nm p) u
-
-keywordMessageAnnotateBlocks :: MethodName -> KeywordMessage -> KeywordMessage
-keywordMessageAnnotateBlocks nm (KeywordMessage k) =
-  KeywordMessage (map (bimap id (keywordArgumentAnnotateBlocks nm)) k)
-
-keywordArgumentAnnotateBlocks :: MethodName -> KeywordArgument -> KeywordArgument
-keywordArgumentAnnotateBlocks nm (KeywordArgument p u b) =
-  KeywordArgument (primaryAnnotateBlocks nm p) u (fmap (map (binaryMessageAnnotateBlocks nm)) b)
-
-cascadedMessagesAnnotateBlocks :: MethodName -> CascadedMessages -> CascadedMessages
-cascadedMessagesAnnotateBlocks nm cm = map (messagesAnnotateBlocks nm) cm
+  let annotate p =
+        case p of
+          PrimaryBlock b -> PrimaryBlock (b {blockMethodName = Just (methodName m)})
+          _ -> p
+  in methodDefinitionRewritePrimary annotate m
---
+++ Language/Smalltalk/Ansi/Rewrite.hs
@@ -0,0 +1,82 @@
+-- | Ast rewriting functions.
+module Language.Smalltalk.Ansi.Rewrite where
+
+import Data.Bifunctor {- base -}
+
+import Language.Smalltalk.Ansi {- stsc3 -}
+
+-- * Rewrite Primary
+
+methodDefinitionRewritePrimary :: (Primary -> Primary) -> MethodDefinition -> MethodDefinition
+methodDefinitionRewritePrimary rw m =
+  m {methodStatements = fmap (statementsRewritePrimary rw) (methodStatements m)}
+
+statementsRewritePrimary :: (Primary -> Primary) -> Statements -> Statements
+statementsRewritePrimary rw st =
+  case st of
+    StatementsReturn ret ->
+      StatementsReturn (returnStatementRewritePrimary rw ret)
+    StatementsExpression expr mst ->
+      StatementsExpression (expressionRewritePrimary rw expr) (fmap (statementsRewritePrimary rw) mst)
+
+returnStatementRewritePrimary :: (Primary -> Primary) -> ReturnStatement -> ReturnStatement
+returnStatementRewritePrimary rw (ReturnStatement expr) =
+  ReturnStatement (expressionRewritePrimary rw expr)
+
+expressionRewritePrimary :: (Primary -> Primary) -> Expression -> Expression
+expressionRewritePrimary rw expr =
+  case expr of
+    ExprAssignment as -> ExprAssignment (assignmentRewritePrimary rw as)
+    ExprBasic be -> ExprBasic (basicExpressionRewritePrimary rw be)
+
+assignmentRewritePrimary :: (Primary -> Primary) -> Assignment -> Assignment
+assignmentRewritePrimary rw (Assignment k expr) =
+  Assignment k (expressionRewritePrimary rw expr)
+
+basicExpressionRewritePrimary :: (Primary -> Primary) -> BasicExpression -> BasicExpression
+basicExpressionRewritePrimary rw (BasicExpression p m cm) =
+  BasicExpression
+  (primaryRewritePrimary rw p)
+  (fmap (messagesRewritePrimary rw) m)
+  (fmap (cascadedMessagesRewritePrimary rw) cm)
+
+primaryRewritePrimary :: (Primary -> Primary) -> Primary -> Primary
+primaryRewritePrimary rw p =
+  case p of
+    PrimaryBlock b -> rw (PrimaryBlock (blockBodyRewritePrimary rw b))
+    PrimaryExpression e -> rw (PrimaryExpression (expressionRewritePrimary rw e))
+    PrimaryArrayExpression a -> rw (PrimaryArrayExpression (map (basicExpressionRewritePrimary rw) a))
+    _ -> rw p
+
+blockBodyRewritePrimary :: (Primary -> Primary) -> BlockBody -> BlockBody
+blockBodyRewritePrimary rw b =
+  b {blockStatements = fmap (statementsRewritePrimary rw) (blockStatements b)}
+
+messagesRewritePrimary :: (Primary -> Primary) -> Messages -> Messages
+messagesRewritePrimary rw m =
+  case m of
+    MessagesUnary u b k ->
+      MessagesUnary u (fmap (map (binaryMessageRewritePrimary rw)) b) (fmap (keywordMessageRewritePrimary rw) k)
+    MessagesBinary b k ->
+      MessagesBinary (map (binaryMessageRewritePrimary rw) b) (fmap (keywordMessageRewritePrimary rw) k)
+    MessagesKeyword k ->
+      MessagesKeyword (keywordMessageRewritePrimary rw k)
+
+binaryMessageRewritePrimary :: (Primary -> Primary) -> BinaryMessage -> BinaryMessage
+binaryMessageRewritePrimary rw (BinaryMessage k a) =
+  BinaryMessage k (binaryArgumentRewritePrimary rw a)
+
+binaryArgumentRewritePrimary :: (Primary -> Primary) -> BinaryArgument -> BinaryArgument
+binaryArgumentRewritePrimary rw (BinaryArgument p u) =
+  BinaryArgument (primaryRewritePrimary rw p) u
+
+keywordMessageRewritePrimary :: (Primary -> Primary) -> KeywordMessage -> KeywordMessage
+keywordMessageRewritePrimary rw (KeywordMessage k) =
+  KeywordMessage (map (bimap id (keywordArgumentRewritePrimary rw)) k)
+
+keywordArgumentRewritePrimary :: (Primary -> Primary) -> KeywordArgument -> KeywordArgument
+keywordArgumentRewritePrimary rw (KeywordArgument p u b) =
+  KeywordArgument (primaryRewritePrimary rw p) u (fmap (map (binaryMessageRewritePrimary rw)) b)
+
+cascadedMessagesRewritePrimary :: (Primary -> Primary) -> CascadedMessages -> CascadedMessages
+cascadedMessagesRewritePrimary rw cm = map (messagesRewritePrimary rw) cm
