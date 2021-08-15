e7844528f75dde6dae152d58d69aebaff608e270
rohan <rd>
2021-08-15 13:15:03 +1000

    ~rem

--- Language/Smalltalk/Ansi/Query.hs
+++ Language/Smalltalk/Ansi/Query.hs
@@ -4,7 +4,7 @@ module Language.Smalltalk.Ansi.Query where
 import Language.Smalltalk.Ansi {- stsc3 -}
 import Language.Smalltalk.Ansi.Fold {- stsc3 -}
 
-{- | Traverse block body for any returning block.
+{- | Traverse block body looking for any return statements.
      Note that a block that has a return statement may not execute it.
 
 > p = stParse blockBody
