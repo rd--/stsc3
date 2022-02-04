d7d8caa6257b535ca6cd298f8d3210e50d82c39e
rohan <rd@slavepianos.org>
2022-02-05 07:33:14 +1100

    ~cgi,+path

--- cgi-bin/stc-to-js-cgi.py
+++ cgi-bin/stc-to-js-cgi.py
@@ -2,13 +2,15 @@
 
 import cgi
 import cgitb
+import os
 import subprocess
 import sys
 
 sys.stdout.write("Content-type: text/plain; charset=utf-8\n\n")
+dir = os.path.dirname(os.path.realpath(__file__))
 fs = cgi.FieldStorage()
 stcText = fs['stc'].value
-process = subprocess.Popen(['stc-to-js'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True)
+process = subprocess.Popen([dir + '/stc-to-js'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True)
 process.stdin.write(stcText);
 jsText = process.communicate()[0]
 sys.stdout.write(jsText)
