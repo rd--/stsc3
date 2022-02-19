c5e9de1433ad7bf464c4499bc0bcf7abdddb48e5
rohan <rd@slavepianos.org>
2022-02-20 09:59:36 +1100

    ~cgi,+cmdinput

--- cgi-bin/README.md
+++ cgi-bin/README.md
@@ -1,10 +1,10 @@
 # cgi-bin
 
-Online .stc to .js translator, see html/stc-to-js.html for form interface.
+Online .stc to .js/.st translators, see html/stc-to-{js, st}.html for form interface.
 
-## stc-to-js.hs
+## stc-to-{js, st}.hs
 
-- Translator from .stc to .js.
+- Translator from .stc to .{js, st}.
 - Required to compile on a limited resource server (ghc 8.6.5, base libraries and split only).
 - See <https://rohandrape.net/pub/stsc3/html/stc-to-js.html>
 
--- cgi-bin/stsc3-cgi.py
+++ cgi-bin/stsc3-cgi.py
@@ -9,8 +9,9 @@ import sys
 sys.stdout.write("Content-type: text/plain; charset=utf-8\n\n")
 dir = os.path.dirname(os.path.realpath(__file__))
 fs = cgi.FieldStorage()
-stcText = fs['stc'].value
-process = subprocess.Popen([dir + '/stc-to-js'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True)
+cmdText = fs.getvalue('cmd', 'stc-to-js');
+stcText = fs.getvalue('stc');
+process = subprocess.Popen([dir + '/' + cmdText], stdin=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True)
 process.stdin.write(stcText);
 jsText = process.communicate()[0]
 sys.stdout.write(jsText)
