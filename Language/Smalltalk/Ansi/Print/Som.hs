ccd38fd9ec4b38cece54563f0e1b8fcec57db6d1
rohan <rd@slavepianos.org>
2022-05-19 01:09:24 +1000

    +somcomments

--- Language/Smalltalk/Ansi/Print/Som.hs
+++ Language/Smalltalk/Ansi/Print/Som.hs
@@ -9,22 +9,34 @@ import qualified Language.Smalltalk.Ansi.Print as St {- stsc3 -}
 somVariablesPrint :: [St.Identifier] -> String
 somVariablesPrint var = if null var then "" else St.temporaries_pp (St.Temporaries var)
 
+{- | Print class definition in Som format.
+
+If there is a class comment provide it as a comment class method returning a String.
+If methods have source code print that rather than pretty-printing the method definition.
+-}
 classDefinitionPrintSom :: St.ClassDefinition -> String
 classDefinitionPrintSom cd =
   let cv = St.classVariableNames cd
       cm = St.classMethods cd
+      cmt txt = unlines ["comment = (^'", txt, "')"]
       ln =
         [unwords [St.className cd, "=", fromMaybe (if St.className cd == "Object" then "nil" else "Object") (St.superclassName cd), "("]
         ,somVariablesPrint (St.classInstanceVariableNames cd)
         ,unlines (map methodDefinitionPrintSom (St.instanceMethods cd))
         ,if null cv && null cm
          then ""
-         else unlines ["----------------------------"
-                      ,somVariablesPrint cv
-                      ,unlines (map methodDefinitionPrintSom cm)]
+         else unlines
+              ["----------------------------"
+              ,somVariablesPrint cv
+              ,unlines (map methodDefinitionPrintSom cm)
+              ,cmt (fromMaybe "" (St.classComment cd))]
         ,")"]
   in unlines (filter (not . null) ln)
 
+{- | Print method definition in Som format.
+
+If methods have source code print that rather than pretty-printing the definition, also for primitives.
+-}
 methodDefinitionPrintSom :: St.MethodDefinition -> String
 methodDefinitionPrintSom md =
   let St.MethodDefinition _ _ pat tmp stm prm _ src = md
