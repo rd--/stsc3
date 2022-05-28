54f5193b3c8f3eaccc28390ce18d1df8cf465349
rohan <rd@slavepianos.org>
2022-05-29 08:59:09 +1000

    nicername

--- Language/Smalltalk/Organisation.hs
+++ Language/Smalltalk/Organisation.hs
@@ -1,3 +1,8 @@
+{- | Class organisation.
+
+Classes and methods each reside in categories.
+Class categories are (by convention) of the form "Major-Minor" forming a two-level hierarchy.
+-}
 module Language.Smalltalk.Organisation where
 
 import Data.Function {- base -}
@@ -5,16 +10,19 @@ import Data.List {- base -}
 
 import Language.Smalltalk.Ansi {- stsc3 -}
 
+-- | Set of all categories in library.
 classLibraryAllCategories :: ClassLibrary -> [String]
 classLibraryAllCategories = sort . nub . map classCategoryOrError
 
-classLibraryHierarchy :: ClassLibrary -> [(String, [String])]
-classLibraryHierarchy cl =
+-- | Set of all categories in library organised as (Major,[Minor]).
+classLibraryCategoryHierarchy :: ClassLibrary -> [(String, [String])]
+classLibraryCategoryHierarchy cl =
   let cp = map classCategoryPartsOrError cl
       grp = groupBy ((==) `on` fst)
       jn l = (fst (head l), map snd l)
       collate = map jn . grp
   in collate (nub (sort cp))
 
+-- | Select class definitions by category.
 classLibraryCategory :: ClassLibrary -> String -> [ClassDefinition]
 classLibraryCategory lib cat = filter ((== cat) . classCategoryOrError) lib
