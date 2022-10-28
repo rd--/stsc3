8010e9410ae11665d9b291e5f693010a4c58f4fd
rohan <rd@slavepianos.org>
2022-10-28 13:10:28 +1100

    ~js/~test

--- js/stc-test.js
+++ js/stc-test.js
@@ -3,17 +3,23 @@
 import { stc } from './stc-grammar.js'
 import { } from './stc-as-stc.js'
 
-[
-    '5', '2.3',
-    '"string"', "'symbol'",
-    'true', 'nil',
-    'p', 'sqrt', 'SinOsc',
-    '(5)', '("x")','(1 + 2)',
-    '[]', '[1]', '[1, 2.3, four]',
-    'i + j', 'i * j + k', 'i + j * k / 2',
-    'p.q', 'p.q.r', 'p.q(r)', 'p.q(r).s(t)', 'p.q(r.s(t))',
-    'SinOsc(440, 0)', 'Float.e',
-    '{ Rand(0, 1) }', '{ var x, y; x = Rand(0, 1); y = [x, x]; y }',
-    '{ arg x; x * x }', '{ arg x, y; (x.squared + y.squared).sqrt }',
-    'var x; x = 9; x.sqrt.postln', 'var x = 9; x.sqrt.postln'
-].forEach(str => console.log(str, '=>', stc.parse(str).asStc))
+stc.test = {};
+
+stc.test.asStc = function() {
+	[
+		'5', '2.3',
+		'"string"', "'symbol'",
+		'true', 'nil',
+		'p', 'sqrt', 'SinOsc',
+		'(5)', '("x")','(1 + 2)',
+		'[]', '[1]', '[1, 2.3, four]',
+		'i + j', 'i * j + k', 'i + j * k / 2',
+		'p.q', 'p.q.r', 'p.q(r)', 'p.q(r).s(t)', 'p.q(r.s(t))',
+		'SinOsc(440, 0)', 'Float.e',
+		'{ Rand(0, 1) }', '{ var x, y; x = Rand(0, 1); y = [x, x]; y }',
+		'{ arg x; x * x }', '{ arg x, y; (x.squared + y.squared).sqrt }',
+		'var x; x = 9; x.sqrt.postln', 'var x = 9; x.sqrt.postln'
+	].forEach(str => console.log(str, '=>', stc.parse(str).asStc));
+};
+
+export { stc } from './stc-grammar.js'
